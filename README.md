c5e5164d3e34ffe097101d1197141fe3a960bf3d
Thien Nguyen <tatthien.contact@gmail.com>
2022-11-18 10:48:53 +0700

    docs: add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+Homebrew Tab for [giraffe](https://github.com/tatthien/giraffe)
+
+```bash
+$ brew tap tatthien/giraffe
+$ brew install giraffe
+```
--- giraffe.rb
+++ giraffe.rb
@@ -2,6 +2,7 @@ class Giraffe < Formula
   desc "🦒 An opinionated static site generator written in Go"
   homepage "https://github.com/tatthien/giraffe"
   url "https://github.com/tatthien/giraffe/releases/download/v0.11.0/giraffe_0.11.0_darwin_amd64.tar.gz"
+  version "0.11.0"
   sha256 "682dee2a7ee88a2111f5c6d74a94834772b9cfa06dc22142b92e7e86dbd3e885"
   license "MIT"
   def install
