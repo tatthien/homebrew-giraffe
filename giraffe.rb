a615f308a12b84212b33be3f375e24c02a9ddc64
Thien Nguyen <tatthien.contact@gmail.com>
2022-11-18 10:13:31 +0700

    v0.10.0

---
+++ giraffe.rb
@@ -0,0 +1,15 @@
+class Giraffe < Formula
+  desc "🦒 An opinionated static site generator written in Go"
+  homepage "https://github.com/tatthien/giraffe"
+  url "https://github.com/tatthien/giraffe/releases/download/v0.10.0/giraffe_0.10.0_darwin_amd64.tar.gz"
+  sha256 "8224d667e3f8811b01914c2cd149c0a6249f1f00289296c11f20b1330999d94b"
+  license "MIT"
+  def install
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    bin.install 'giraffe'
+  end
+
+  test do
+    system "false"
+  end
+end
