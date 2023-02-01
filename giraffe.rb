af6213acb873b11e2579c21ef5f52ce4f4a3b70c
Thien Nguyen <tatthien.contact@gmail.com>
2023-02-01 11:34:03 +0700

    v0.12.0

--- giraffe.rb
+++ giraffe.rb
@@ -1,9 +1,9 @@
 class Giraffe < Formula
   desc "🦒 An opinionated static site generator written in Go"
   homepage "https://github.com/tatthien/giraffe"
-  url "https://github.com/tatthien/giraffe/releases/download/v0.11.0/giraffe_0.11.0_darwin_amd64.tar.gz"
-  version "0.11.0"
-  sha256 "682dee2a7ee88a2111f5c6d74a94834772b9cfa06dc22142b92e7e86dbd3e885"
+  url "https://github.com/tatthien/giraffe/releases/download/v0.12.0/giraffe_0.12.0_darwin_amd64.tar.gz"
+  version "0.12.0"
+  sha256 "3bad50ba0ba345bf1347c85a54c5f325ed58e16824e9b5123bab77d211a9bd91"
   license "MIT"
   def install
     bin.install 'giraffe'
